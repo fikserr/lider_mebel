e6690e2f0a0d2176d5df39d333c166216ead705f
fikserr <azizxon7777777@gmail.com>
2025-12-26 10:59:16 +0500

    finish 2

--- public/build/assets/index-CdtnVi-6.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-0nnjcZrn.js";var r=t.createContext(void 0);function o(o){const e=t.useContext(r);return o||e||"ltr"}export{o as u};
---
+++ public/build/assets/logoLM-CpOk4V0X.js
@@ -0,0 +1,1 @@
+const s="/build/assets/logoLM-C-2Yd83v.png";export{s as L};
